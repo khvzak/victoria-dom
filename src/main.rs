6501b5b48581fe1ed4ef06ec4d112b3695d481d5
Aleksandr Orlenko <zxteam@gmail.com>
2016-03-27 14:56:33 +0300

    unimportant changes in main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 extern crate dom_parser;
 //extern crate regex;
 
+//use dom_parser::dom;
 use dom_parser::dom::html;
-//use dom_parser::dom::css;
+use dom_parser::dom::css;
 
 //use std::char;
 //use regex::Regex;
@@ -13,7 +14,7 @@ fn main() {
     let html = r#"<div><p id="a">Test</p><p id="b">123</p></div>"#;
     let tree = dom_parser::dom::html::parse(html);
 
-    //let node_selected = dom_parser::dom::css::select_one(&tree, "div > p[id=a]").unwrap();
+    let node_selected = dom_parser::dom::css::select_one(&tree, "div > p[id=a]").unwrap();
 
     //println!("{:?}", node_selected);
     //println!("{}", dom_parser::dom::html::render(&node_selected));
@@ -27,8 +28,10 @@ fn main() {
     println!("{}", html_rendered);
     */
 
-    //let x = dom_parser::dom::css::_parse("body > div > div[foo=bar]");
+    //let x = dom_parser::dom::css::parse("body > div > div[foo=bar]");
     //println!("{:?}", x);
+
+    //println!("{:?}", dom_parser::dom)
 
     //println!("{:?}", _test());
     //println!("{:?}", _test());
