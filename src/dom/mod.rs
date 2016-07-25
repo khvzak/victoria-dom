568990c93e5ea8b28e22b01d4cac98297a59d520
Aleksandr Orlenko <zxteam@gmail.com>
2016-07-25 20:32:53 +0100

    Add base documentation

--- src/dom/mod.rs
+++ src/dom/mod.rs
@@ -8,15 +8,31 @@ use regex::Regex;
 
 use self::html::TreeNode;
 
+/// The HTML `DOM` type
 pub struct DOM {
     tree: Rc<TreeNode>,
 }
 
 impl DOM {
+    /// Construct a new `DOM` object and parse HTML.
+    ///
+    /// ```
+    /// use victoria_dom::DOM;
+    /// let dom = DOM::new("<div id=\"title\">Hello</div>");
+    /// ```
     pub fn new(html: &str) -> DOM {
         DOM { tree: html::parse(html) }
     }
 
+    /// Find all ancestor elements of the current element matching the optional CSS selector
+    /// and return a Vector of DOM objects of these elements.
+    ///
+    /// ```
+    /// use victoria_dom::DOM;
+    /// let dom = DOM::new("<html><body><div id=\"title\">Hello</div></body></html>");
+    /// let ancestors: Vec<_> = dom.at("div").unwrap().ancestors(None).iter().map(|x| x.tag().unwrap().to_string()).collect();
+    /// assert_eq!(ancestors, ["body", "html"]);
+    /// ```
     pub fn ancestors(&self, selector: Option<&str>) -> Vec<DOM> {
         let mut ancestors = Vec::new();
         let mut node = self.tree.clone();
@@ -29,6 +45,8 @@ impl DOM {
         ancestors
     }
 
+    /// Find first descendant element of the current element matching the CSS selector and return it as a DOM object,
+    /// or `None` if none could be found.
     pub fn at(&self, selector: &str) -> Option<DOM> {
         if let Some(node) = css::select_one(&self.tree, selector) {
             return Some(DOM { tree: node })
@@ -36,18 +54,29 @@ impl DOM {
         None
     }
 
+    /// The current element tag name.
     pub fn tag(&self) -> Option<&str> {
         self.tree.get_tag_name()
     }
 
+    /// The current element attribute2value map.
     pub fn attrs(&self) -> BTreeMap<String, Option<String>> {
         self.tree.get_tag_attrs().map_or_else(|| BTreeMap::new(), |x| x.clone())
     }
 
+    /// The current element attribute value, or `None` if there are no attribute with the name or value.
     pub fn attr(&self, name: &str) -> Option<&str> {
         self.tree.get_tag_attrs().and_then(|x| x.get(name)).and_then(|x| x.as_ref()).map(|x| x.as_str())
     }
 
+    /// Find all child elements of the current element matching the CSS selector and return a Vector of DOM objects of these elements.
+    ///
+    /// ```
+    /// use victoria_dom::DOM;
+    /// let dom = DOM::new("<div><div id=\"a\">A <span id=\"c\">C</span></div><div id=\"b\">B</div></div>");
+    /// let childs: Vec<_> = dom.at("div").unwrap().childs(None).iter().map(|x| x.attr("id").unwrap().to_string()).collect();
+    /// assert_eq!(childs, ["a", "b"]);
+    /// ```
     pub fn childs(&self, selector: Option<&str>) -> Vec<DOM> {
         self.tree.get_childs().unwrap_or(Vec::new()).into_iter().filter_map(|x|
             if x.is_tag() && (selector.is_none() || css::matches(&x, selector.unwrap())) {
@@ -58,30 +87,57 @@ impl DOM {
         ).collect()
     }
 
+    /// Find all descendant elements of the current element matching the CSS selector and return a Vector of DOM objects of these elements.
+    ///
+    /// ```
+    /// use victoria_dom::DOM;
+    /// let dom = DOM::new("<div><div id=\"a\"><div id=\"c\">C</div></div><div id=\"b\">B</div></div>");
+    /// let elems: Vec<_> = dom.find("div[id]").iter().map(|x| x.attr("id").unwrap().to_string()).collect();
+    /// assert_eq!(elems, ["a", "c", "b"]);
+    /// ```
     pub fn find(&self, selector: &str) -> Vec<DOM> {
         css::select(&self.tree, selector, 0).into_iter().map(|x| DOM { tree: x }).collect()
     }
 
+    /// Check if the current element matches the CSS selector.
     pub fn matches(&self, selector: &str) -> bool {
         css::matches(&self.tree, selector)
     }
 
+    /// Find all sibling elements after the current element matching the CSS selector and return a Vector of DOM objects of these elements.
+    ///
+    /// ```
+    /// use victoria_dom::DOM;
+    /// let dom = DOM::new("<div><div id=\"a\"><div id=\"c\">C</div></div><div id=\"b\">B</div></div>");
+    /// let elems: Vec<_> = dom.at("div#a").unwrap().following(None).iter().map(|x| x.attr("id").unwrap().to_string()).collect();
+    /// assert_eq!(elems, ["b"]);
+    /// ```
     pub fn following(&self, selector: Option<&str>) -> Vec<DOM> {
         self._siblings().into_iter().skip_while(|x| x.id != self.tree.id).skip(1)
             .filter(|x| selector.is_none() || css::matches(x, selector.unwrap()))
             .map(|x| DOM { tree: x }).collect()
     }
 
+    /// Return a DOM object for next sibling element, or `None` if there are no more siblings.
     pub fn next(&self) -> Option<DOM> {
         self._siblings().into_iter().skip_while(|x| x.id != self.tree.id).skip(1).next().map(|x| DOM { tree: x })
     }
 
+    /// Find all sibling elements before the current element matching the CSS selector and return a Vector of DOM objects of these elements.
+    ///
+    /// ```
+    /// use victoria_dom::DOM;
+    /// let dom = DOM::new("<div><div id=\"a\"><div id=\"c\">C</div></div><div id=\"b\">B</div></div>");
+    /// let elems: Vec<_> = dom.at("div#b").unwrap().preceding(None).iter().map(|x| x.attr("id").unwrap().to_string()).collect();
+    /// assert_eq!(elems, ["a"]);
+    /// ```
     pub fn preceding(&self, selector: Option<&str>) -> Vec<DOM> {
         self._siblings().into_iter().take_while(|x| x.id != self.tree.id)
             .filter(|x| selector.is_none() || css::matches(x, selector.unwrap()))
             .map(|x| DOM { tree: x }).collect()
     }
 
+    /// Return a DOM object for the previous sibling element, or `None` if there are no more siblings.
     pub fn prev(&self) -> Option<DOM> {
         self._siblings().into_iter().take_while(|x| x.id != self.tree.id).last().map(|x| DOM { tree: x })
     }
@@ -93,26 +149,56 @@ impl DOM {
             .unwrap_or(Vec::new())
     }
 
+    /// Return a DOM object for the parent of the current element, or `None` if this element has no parent.
     pub fn parent(&self) -> Option<DOM> {
         self.tree.get_parent().map(|x| DOM { tree: x })
     }
 
+    /// Render the current element and its content to HTML.
     pub fn to_string(&self) -> String {
         html::render(&self.tree)
     }
 
+    /// Extract text content from the current element only (not including child elements) with smart whitespace trimming.
+    ///
+    /// ```
+    /// use victoria_dom::DOM;
+    /// let dom = DOM::new("<div>foo\n<p>bar</p>baz\n</div>");
+    /// assert_eq!(dom.at("div").unwrap().text(), "foo baz");
+    /// ```
     pub fn text(&self) -> String {
         self._text(false, true) // non-recursive trimmed
     }
 
+    /// Extract text content from the current element only (not including child elements) without smart whitespace trimming.
+    ///
+    /// ```
+    /// use victoria_dom::DOM;
+    /// let dom = DOM::new("<div>foo\n<p>bar</p>baz\n</div>");
+    /// assert_eq!(dom.at("div").unwrap().rtext(), "foo\nbaz\n");
+    /// ```
     pub fn rtext(&self) -> String {
         self._text(false, false) // non-recursive raw
     }
 
+    /// Extract text content from all descendant nodes of the current element with smart whitespace trimming.
+    ///
+    /// ```
+    /// use victoria_dom::DOM;
+    /// let dom = DOM::new("<div>foo\n<p>bar</p>baz\n</div>");
+    /// assert_eq!(dom.at("div").unwrap().text_all(), "foo bar baz");
+    /// ```
     pub fn text_all(&self) -> String {
         self._text(true, true) // recursive trimmed
     }
 
+    /// Extract text content from all descendant nodes of the current element with smart whitespace trimming.
+    ///
+    /// ```
+    /// use victoria_dom::DOM;
+    /// let dom = DOM::new("<div>foo\n<p>bar</p>baz\n</div>");
+    /// assert_eq!(dom.at("div").unwrap().rtext_all(), "foo\nbarbaz\n");
+    /// ```
     pub fn rtext_all(&self) -> String {
         self._text(true, false) // recursive raw
     }
@@ -139,6 +225,13 @@ impl DOM {
         }
     }
 
+    /// Return content of the current element.
+    ///
+    /// ```
+    /// use victoria_dom::DOM;
+    /// let dom = DOM::new("<div><b>Test</b></div>");
+    /// assert_eq!(dom.at("div").unwrap().content(), "<b>Test</b>");
+    /// ```
     pub fn content(&self) -> String {
         self.tree.get_childs().unwrap().into_iter().map(|x| html::render(&x)).collect::<Vec<_>>().join("")
     }
@@ -169,7 +262,7 @@ fn _nodes_text(nodes: &Vec<Rc<TreeNode>>, recursive: bool, trim: bool) -> String
         };
 
         // Add leading whitespace if punctuation allows it
-        if _RE2.is_match(&text) && _RE3.is_match(&chunk) {
+        if trim && _RE2.is_match(&text) && _RE3.is_match(&chunk) {
             chunk = " ".to_owned() + &chunk
         }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,63 @@
+// lib.rs
+// This crate is a library
+#![crate_type = "lib"]
+// The library is named "victoria_dom"
+#![crate_name = "victoria_dom"]
+
+//! Minimalistic HTML parser with CSS selectors
+//!
+//! The project has been inspired by [Mojo::DOM](https://metacpan.org/pod/Mojo::DOM).
+//!
+//! It will even try to interpret broken HTML, so you should not use it for validation.
+//!
+//! # Examples
+//!
+//! ```
+//! extern crate victoria_dom;
+//!
+//! use victoria_dom::DOM;
+//!
+//! fn main() {
+//!     let html = r#"<html><div id="main">Hello, <a href="http://rust-lang.org" alt="The Rust Programing Language">Rust</a></div></html>"#;
+//!     let dom = DOM::new(html);
+//!
+//!     assert_eq!(dom.at("div#main > a").unwrap().attr("alt").unwrap(), "The Rust Programing Language");
+//! }
+//! ```
+//!
+//! # Supported CSS selectors
+//!
+//! * `*` Any element.
+//! * `E` An element of type `E`.
+//! * `E[foo]` An `E` element with a `foo` attribute.
+//! * `E[foo="bar"]` An `E` element whose `foo` attribute value is exactly equal to `bar`.
+//! * `E[foo~="bar"]` An `E` element whose `foo` attribute value is a list of whitespace-separated values, one of which is exactly equal to `bar`.
+//! * `E[foo^="bar"]` An `E` element whose `foo` attribute value begins exactly with the string `bar`.
+//! * `E[foo$="bar"]` An `E` element whose `foo` attribute value ends exactly with the string `bar`.
+//! * `E[foo*="bar"]` An `E` element whose `foo` attribute value contains the substring `bar`.
+//! * `E:root` An `E` element, root of the document.
+//! * `E:nth-child(n)` An `E` element, the `n-th` child of its parent.
+//! * `E:nth-last-child(n)` An `E` element, the `n-th` child of its parent, counting from the last one.
+//! * `E:nth-of-type(n)` An `E` element, the `n-th` sibling of its type.
+//! * `E:nth-last-of-type(n)` An `E` element, the `n-th` sibling of its type, counting from the last one.
+//! * `E:first-child` An `E` element, first child of its parent.
+//! * `E:last-child` An `E` element, last child of its parent.
+//! * `E:first-of-type` An `E` element, first sibling of its type.
+//! * `E:last-of-type` An `E` element, last sibling of its type.
+//! * `E:only-child` An `E` element, only child of its parent.
+//! * `E:only-of-type` An `E` element, only sibling of its type.
+//! * `E:empty` An `E` element that has no children (including text nodes).
+//! * `E:checked` A user interface element `E` which is checked (for instance a radio-button or checkbox).
+//! * `E.warning` An `E` element whose class is `warning`.
+//! * `E#myid` An `E` element with ID equal to `myid`.
+//! * `E:not(s)` An `E` element that does not match simple selector `s`.
+//! * `E F` An `F` element descendant of an `E` element.
+//! * `E > F` An `F` element child of an `E` element.
+//! * `E + F` An `F` element immediately preceded by an `E` element.
+//! * `E ~ F` An `F` element preceded by an `E` element.
+//! * `E, F, G` Elements of type `E`, `F` and `G`.
+//! * `E[foo=bar][bar=baz]` An `E` element whose attributes match all following attribute selectors.
+
 #[macro_use] extern crate lazy_static;
 #[macro_use] extern crate maplit;
 extern crate regex;
@@ -5,5 +65,5 @@ extern crate uuid;
 
 pub use dom::DOM;
 
-pub mod dom;
-pub mod util;
+mod dom;
+mod util;
