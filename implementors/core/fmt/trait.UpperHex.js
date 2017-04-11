4bc678c8cffc5d5faa300e278615a0fd1031a0ff
Travis CI User <travis@example.org>
2017-04-11 16:50:46 +0000

    Update documentation

---
+++ implementors/core/fmt/trait.UpperHex.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["victoria_dom"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/rand/trait.Rand.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["victoria_dom"] = ["impl <a class='trait' href='https://doc.rust-lang.org/rand/rand/trait.Rand.html' title='rand::Rand'>Rand</a> for <a class='struct' href='https://doc.rust-lang.org/uuid/uuid/struct.Uuid.html' title='uuid::Uuid'>Uuid</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
