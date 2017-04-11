111999df0d9a628b1c01da57483cb8fef12c295e
Aleksandr Orlenko <zxteam@gmail.com>
2017-04-11 15:33:13 +0100

    Coverage support

---
+++ ci/after_success.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# This runs only when a commit is pushed to master. It is responsible for
+# updating docs and computing coverage statistics.
+
+set -e
+
+if [ "$TRAVIS_RUST_VERSION" != "stable" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
+  exit 0
+fi
+
+env
+
+# Build and upload docs.
+echo '<meta http-equiv=refresh content=0;url=regex/index.html>' > target/doc/index.html
+ve=$(mktemp -d)
+virtualenv "$ve"
+"$ve"/bin/pip install --upgrade pip
+"$ve"/bin/pip install ghp-import
+"$ve"/bin/ghp-import -n target/doc
+#git push -qf https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+
+# Install kcov.
+tmpdir=$(mktemp -d)
+pushd "$tmpdir"
+wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
+tar zxf master.tar.gz
+mkdir kcov-master/build
+cd kcov-master/build
+cmake ..
+make
+make install DESTDIR="$tmpdir"
+popd
+
+PATH="$tmpdir/usr/local/bin:$PATH"
+
+# Calculate and upload code coverage.
+tmpdir=$(mktemp -d)
+cargo test --no-run --verbose
+find ./target/debug -maxdepth 1 -type f -executable -print | xargs -n 1 kcov --exclude-pattern=/.cargo --verify "$tmpdir"
+kcov --verify --coveralls-id=$TRAVIS_JOB_ID --merge target/cov "$tmpdir"/*
