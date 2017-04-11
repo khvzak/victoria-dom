5ba138a183d1ea8b1d63da091f9043df5bdc3346
Aleksandr Orlenko <zxteam@gmail.com>
2017-04-11 17:12:57 +0100

    Travis CI support

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # victoria-dom
 Minimalistic HTML parser with CSS selectors
 
+[![](http://meritbadge.herokuapp.com/victoria-dom)](https://crates.io/crates/victoria-dom)
 [![MIT licensed](https://img.shields.io/badge/license-MIT-blue.svg)](./LICENSE)
+[![Build Status](https://travis-ci.org/khvzak/victoria-dom.svg?branch=master)](https://travis-ci.org/khvzak/victoria-dom)
+[![Coverage Status](https://coveralls.io/repos/github/khvzak/victoria-dom/badge.svg?branch=master)](https://coveralls.io/github/khvzak/victoria-dom?branch=master)
 
 The project has been inspired by [Mojo::DOM](https://metacpan.org/pod/Mojo::DOM).
 
---
+++ ci/after_success.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# This runs only when a commit is pushed to master. It is responsible for
+# updating docs and computing coverage statistics.
+
+set -e
+
+if [ "$TRAVIS_RUST_VERSION" != "stable" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
+  exit 0
+fi
+
+# Build and upload docs.
+cargo doc --no-deps --verbose
+echo '<meta http-equiv=refresh content=0;url=victoria_dom/index.html>' > target/doc/index.html
+ve=$(mktemp -d)
+virtualenv "$ve"
+"$ve"/bin/pip install --upgrade pip
+"$ve"/bin/pip install ghp-import
+"$ve"/bin/ghp-import -n target/doc
+git push -qf https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+
+# Install kcov.
+tmpdir=$(mktemp -d)
+pushd "$tmpdir"
+wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
+tar zxf master.tar.gz
+mkdir kcov-master/build
+cd kcov-master/build
+cmake ..
+make
+make install DESTDIR="$tmpdir"
+popd
+
+PATH="$tmpdir/usr/local/bin:$PATH"
+
+# Calculate and upload code coverage.
+tmpdir=$(mktemp -d)
+cargo test --no-run --verbose
+
+find ./target/debug -maxdepth 1 -type f -executable | \
+  xargs -n 1 basename | \
+  xargs -n 1 -I CMD \
+    kcov --exclude-pattern=/.cargo --verify "$tmpdir/CMD" ./target/debug/CMD
+
+kcov --verify --coveralls-id=$TRAVIS_JOB_ID --merge target/cov "$tmpdir"/*
